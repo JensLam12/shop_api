5d2060986b1418835e53b8545cd6a2b2174e14da
JensLam12 <jesterlynn1986@gmail.com>
2023-07-09 18:22:50 -0700

    Added changes in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(
-      
+
     ),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -20,6 +22,9 @@ import { FilesModule } from './files/files.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join( __dirname, '..', 'public')
     }),
     ProductsModule,
     CommonModule,
